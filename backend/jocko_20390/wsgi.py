282fef9da888f96a977a9fb251fa2f88b8c93bb4
Crowdbotics <team@crowdbotics.com>
2020-09-17 20:15:29 +0000

    Update models

--- backend/jocko_20390/wsgi.py
+++ backend/jocko_20390/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jocko_20390.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jocko_20390.settings")
 
 application = get_wsgi_application()
